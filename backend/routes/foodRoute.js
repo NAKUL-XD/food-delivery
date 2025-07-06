6cf4b53816d35b1ec853b690b209f18d83b5b688
NAKUL-XD <nakullegendary@gmail.com>
2025-07-06 13:19:08 +0530

    Update foodRoute.js

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,26 +1,22 @@
 import express from 'express';
-import { addFood, listFood , removeFood} from '../controllers/foodController.js'; 
+import { addFood, listFood, removeFood } from '../controllers/foodController.js';
+import authMiddleware from "../middleware/auth.js";
 import multer from 'multer';
 
+const foodRouter = express.Router();
 
-const foodRouter = express.Router(
-);
-//image storqage
+// image storage
 const storage = multer.diskStorage({
-    destination: "uploads",
-    filename: (req, file, cb) => {
-       return  cb(null, `${Date.now()}-${file.originalname}`);
-    }
+  destination: "uploads",
+  filename: (req, file, cb) => {
+    return cb(null, `${Date.now()}-${file.originalname}`);
+  },
 });
 
 const upload = multer({ storage: storage });
 
-
-foodRouter.post("/add", upload.single("image"), addFood);
-foodRouter.get("/list", listFood); 
-foodRouter.post("/remove", removeFood); 
-
-
-
+foodRouter.post("/add", authMiddleware, upload.single("image"), addFood);
+foodRouter.get("/list", listFood);
+foodRouter.post("/remove", authMiddleware, removeFood);
 
 export default foodRouter;
